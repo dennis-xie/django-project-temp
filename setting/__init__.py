70d253247b4ae8e6373a2d025b8dd3b12cc9b185
dennis-xie <xmlgdhyda@163.com>
2020-01-07 22:54:58 +0800

    Update __init__.py

--- setting/__init__.py
+++ setting/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import absolute_import
+from django.conf import settings
 
-from .celery import  app as celery_app
+if settings.IS_USE_CELERY:
+    from .celery import  app as celery_app
